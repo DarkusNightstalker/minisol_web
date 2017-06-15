677ce282b807e8133473ddb2dd4e9c7d3770342e
Jhoan Brayam <Jhoan Brayam@Jhoan-PC>
2017-06-15 23:04:15 +0200

    JAX-RS config

--- src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -29,6 +29,10 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(cs.bms.servlet.ResourceActor.class);
+        resources.add(cs.bms.servlet.ResourceISM.class);
+        resources.add(cs.bms.servlet.ResourceSale.class);
+        resources.add(cs.bms.servlet.ResourceSynchro.class);
     }
     
 }
